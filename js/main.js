c3df18b14c0bda217c8e37d72cad53129a4e510c
Ahmet <ahmetcanlyev@gmail.com>
2023-12-13 23:21:38 +0500

    search page

--- js/main.js
+++ js/main.js
@@ -1,34 +1,34 @@
-var menu = document.getElementById("menu");
-var menuBtn = document.getElementById("menuBtn");
-var body = document.body;
+// var menu = document.getElementById("menu");
+// var menuBtn = document.getElementById("menuBtn");
+// var body = document.body;
 
-const closeMenu = () => {
-  menu.classList.remove("active");
-  menuBtn.classList.remove("active");
-  body.classList.remove("active");
-};
+// const closeMenu = () => {
+//   menu.classList.remove("active");
+//   menuBtn.classList.remove("active");
+//   body.classList.remove("active");
+// };
 
-menuBtn.onclick = function () {
-  menu.classList.toggle("active");
-  menuBtn.classList.toggle("active");
-  body.classList.toggle("active");
-};
-window.onclick = function (event) {
-  if (event.target == menu) {
-    closeMenu();
-  }
-};
+// menuBtn.onclick = function () {
+//   menu.classList.toggle("active");
+//   menuBtn.classList.toggle("active");
+//   body.classList.toggle("active");
+// };
+// window.onclick = function (event) {
+//   if (event.target == menu) {
+//     closeMenu();
+//   }
+// };
 
 const header = document.getElementById("header");
 if (header)
   window.addEventListener("scroll", () => {
-    if (document.documentElement.scrollTop > 400) {
+    if (document.documentElement.scrollTop > 250) {
       header.classList.add("sticky");
     } else {
       header.classList.remove("sticky");
     }
   });
-  
+
 // const links = document.querySelectorAll(".links");
 // const sections = document.querySelectorAll(".anchor");
 // function changeLinkState() {
@@ -65,37 +65,36 @@ if (header)
 // new WOW().init({
 //   boxClass: "wow",
 // });
-// const tabBtn = document.querySelectorAll(".tabBtn");
-// const tabEvent = document.querySelectorAll(".tabEvent");
-// tabBtn.forEach((e) => {
-//   onTabClick(tabBtn, tabEvent, e);
-// });
-// function onTabClick(tabBtns, tabItems, item) {
-//   item.addEventListener("click", function (e) {
-//     let currentBtn = item;
-//     let tabId = currentBtn.getAttribute("data-tab");
-//     let currentTab = document.querySelector(tabId);
-//     if (currentBtn.classList.contains("active")) {
-//       console.log("now active");
-//       const faq = currentBtn.parentElement.querySelector(".tabEvent");
-//       if (faq) {
-//         faq.classList.remove("active");
-//         currentBtn.classList.remove("active");
-//       }
-//     } else if (!currentBtn.classList.contains("active")) {
-//       tabBtns.forEach(function (item) {
-//         item.classList.remove("active");
-//       });
+const tabBtn = document.querySelectorAll(".tabBtn");
+const tabEvent = document.querySelectorAll(".tabEvent");
+tabBtn.forEach((e) => {
+  onTabClick(tabBtn, tabEvent, e);
+});
+function onTabClick(tabBtns, tabItems, item) {
+  item.addEventListener("click", function (e) {
+    let currentBtn = item;
+    let tabId = currentBtn.getAttribute("data-tab");
+    let currentTab = document.querySelector(tabId);
+    if (currentBtn.classList.contains("active")) {
+      console.log("now active");
+      const faq = currentBtn.parentElement.querySelector(".tabEvent");
+      if (faq) {
+        faq.classList.remove("active");
+        currentBtn.classList.remove("active");
+      }
+    } else if (!currentBtn.classList.contains("active")) {
+      tabBtns.forEach(function (item) {
+        item.classList.remove("active");
+      });
 
-//       tabItems.forEach(function (item) {
-//         item.classList.remove("active");
-//       });
-//       currentBtn.classList.add("active");
-//       currentTab.classList.add("active");
-//     }
-//   });
-// }
-
+      tabItems.forEach(function (item) {
+        item.classList.remove("active");
+      });
+      currentBtn.classList.add("active");
+      currentTab.classList.add("active");
+    }
+  });
+}
 
 // video ----------------
 // const video = document.querySelector("#video");
